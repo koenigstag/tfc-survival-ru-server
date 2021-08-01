c96296e3dde8ddc25316826181a0bf53d8314239
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-02 00:17:06 +0300

    Install cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const errorHandlers = require('./middlewares/error.handlers');
 const router = require('./routes');
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
