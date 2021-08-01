508968528b1ccc860cd36728512268b44655db58
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-02 00:17:23 +0300

    Add getUser route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,10 @@
-const { User } = require('../db/models/')
+const { User } = require('../db/models/');
 
 module.exports.createUser = async (req, res, next) => {
   try {
-    const { body: { user } } = req;
+    const {
+      body: { user },
+    } = req;
 
     const newUser = await User.create(user);
 
@@ -12,8 +14,23 @@ module.exports.createUser = async (req, res, next) => {
     //delete newUser.dataValues.password;
 
     res.status(200).send({ data: newUser });
+  } catch (e) {
+    next(e);
   }
-  catch (e) {
-    next(e)
+};
+
+module.exports.getUser = async (req, res, next) => {
+  try {
+    const {
+      params: { nickname, token },
+    } = req;
+
+    console.log(nickname);
+
+    const user = await User.findOne({ where: { nickname } });
+
+    res.status(200).send({ data: user });
+  } catch (e) {
+    next(e);
   }
-}
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
-const express = require('express')
-const { createUser } = require('../controllers/user.controller.js')
+const express = require('express');
+const { createUser, getUser } = require('../controllers/user.controller.js');
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/users').get((req, res, next) => {
-  res.send({data: { list: 'Users: ' + (Math.random() * 30) } })
-})
+router.route('/users/:nickname/:token').get(getUser);
 
 router.route('/users').post(createUser);
 
-module.exports = router
+module.exports = router;
