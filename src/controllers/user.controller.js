f3edfcdaf2f79a7a27fb68efa449169f6a3adaa9
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-07 19:51:17 +0300

    Add empty errors

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const createError = require('http-errors');
+const { EmptyResultError } = require('sequelize');
 const { User } = require('../db/models/');
 
 module.exports.createUser = async (req, res, next) => {
@@ -15,13 +17,13 @@ module.exports.createUser = async (req, res, next) => {
     );
 
     if (!newUser) {
-      throw new Error('Cannot create user');
+      return next(new EmptyResultError('Cant create user with that data'));
     }
     //delete newUser.dataValues.password;
 
     res.status(200).send({ data: newUser });
   } catch (e) {
-    next(e)
+    next(e);
   }
 };
 
@@ -33,8 +35,13 @@ module.exports.getUser = async (req, res, next) => {
 
     const user = await User.findOne({ where: { nickname } });
 
+    if (!user) {
+      return next(new EmptyResultError('Cant find user with given nickname'));
+    }
+
     res.status(200).send({ data: user });
   } catch (e) {
+    console.dir(e);
     next(e);
   }
 };
