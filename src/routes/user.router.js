26ca49d35c2db94030170640ffbd5241a369a14b
Evhenii Lapshinov <koenigstag@gmail.com>
2021-11-19 22:45:57 +0200

    Rename variable

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,27 +2,22 @@ const router = require('express').Router();
 // const { checkTokens } = require('../middlewares/checkTokens');
 const decryptPassTransfer = require('../middlewares/decryptPassTransfer');
 const { checkAccessToken } = require('../middlewares/token.mw');
-const {
-  registerUser,
-  getUser,
-  loginUser,
-  changePass,
-  linkDiscord,
-  deleteUser,
-} = require('../controllers/user.controller.js');
+const UserController = require('../controllers/user.controller.js');
 
-router.route('/').post(decryptPassTransfer, registerUser);
+router.route('/').post(decryptPassTransfer, UserController.registerUser);
 
 router
   .route('/:nickname')
-  .post(decryptPassTransfer, checkAccessToken, loginUser)
-  .patch(decryptPassTransfer, checkAccessToken, changePass)
+  .post(decryptPassTransfer, checkAccessToken, UserController.loginUser)
+  .patch(decryptPassTransfer, checkAccessToken, UserController.changePass)
   .delete(checkAccessToken, deleteUser);
 
 // TODO add security middlewares
-// router.route('/:nickname/:accessToken').get(checkAccessToken, getUser);
+// router.route('/:nickname/:accessToken').get(checkAccessToken, UserController.getUser);
 
 // TODO add security middlewares
-router.route('/discord/:nickname').patch(checkAccessToken, linkDiscord);
+router
+  .route('/discord/:nickname')
+  .patch(checkAccessToken, UserController.linkDiscord);
 
 module.exports = router;
