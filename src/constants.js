9721851ba63f61bad4cb914d7a10c7a01ec7ada0
Evhenii Lapshinov <freshjs.fte@gmail.com>
2022-01-06 21:52:40 +0200

    Add env vars

--- src/constants.js
+++ src/constants.js
@@ -6,8 +6,11 @@ const {
     REFRESH_TOKEN_TIME,
     ADMIN_TOKEN_SECRET,
     ADMIN_TOKEN_TIME,
+    NODE_ENV,
   }
 } = process;
+
+const mode = NODE_ENV || 'development';
 
 module.exports = {
   REFRESH_TOKEN_SECRET,
@@ -18,5 +21,9 @@ module.exports = {
   ADMIN_TOKEN_TIME,
 
   MAX_DEVICE_AMOUNT: 3,
-  SALT_ROUNDS: 6
+  SALT_ROUNDS: 6,
+  MAIL_EXPIRATION_TIME: 5 * 60 * 1000, // 5 min
+
+  REDIRECT_URL: mode === 'development' ? 'http://localhost:3000' : 'http://tfc-survival.ru',
+  ORIGIN_URL: mode === 'development' ? 'http://localhost:5001' : 'http://tfc-survival.ru:5000'
 }
