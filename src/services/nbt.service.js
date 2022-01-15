47af7634dd99b8f225a4e140968133e15f73c687
Yevhenii <koenigstag@gmail.com>
2022-01-15 18:22:47 +0200

    Add nick field to response

--- src/services/nbt.service.js
+++ src/services/nbt.service.js
@@ -1,7 +1,10 @@
-const fs = require('fs/promises');
-const path = require('path');
-const nbtModule = require('nbt');
-const { PLAYERS_DATA_PATH, PLAYERS_STATS_PATH } = require('../constants');
+const fs = require("fs/promises");
+const nbtModule = require("nbt");
+const {
+  SERVER_FOLDER,
+  PLAYERS_DATA_PATH,
+  PLAYERS_STATS_PATH,
+} = require("../constants");
 
 module.exports.getUsersData = async () => {
   const files = await fs.readdir(PLAYERS_DATA_PATH);
@@ -25,6 +28,7 @@ module.exports.getUsersData = async () => {
 
 module.exports.getUsersStats = async () => {
   const files = await fs.readdir(PLAYERS_STATS_PATH);
+  const nicks = JSON.parse(await fs.readFile(`${SERVER_FOLDER}/usernamecache.json`, 'utf-8'));
 
   const playersStats = [];
 
@@ -32,7 +36,10 @@ module.exports.getUsersStats = async () => {
     for (const file of files) {
       const content = await fs.readFile(`${PLAYERS_STATS_PATH}/${file}`);
 
-      playersStats.push(JSON.parse(content));
+      playersStats.push({
+        ...JSON.parse(content),
+        nickname: nicks[file.replace(".json", "")],
+      });
     }
   }
 
