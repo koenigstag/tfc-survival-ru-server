950ed163f6cd911c4792f93645044965973ba2b3
Evhenii Lapshinov <koenigstag@gmail.com>
2022-01-15 11:17:17 +0200

    Fix port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { log } = require('./misc/logger');
 const app = require('./app.js');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
 
 const server = http.createServer(app);
 
