c4a6831e3417a17cc8c9ef1c759e5a4e2af8829a
Yevhenii <koenigstag@gmail.com>
2022-01-15 16:06:42 +0200

    Refactor db user and pass

--- src/config/database.js
+++ src/config/database.js
@@ -1,29 +1,32 @@
-const path = require('path');
+const path = require("path");
+const NODE_ENV = process.env.NODE_ENV;
+const DB_USER = process.env["DB_USER_" + NODE_ENV];
+const DB_PASSWORD = process.env["DB_PASSWORD_" + NODE_ENV];
 
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'postgres',
-    database: 'tfc_survival_development',
-    host: 'localhost',
-    dialect: 'postgres',
-    migrationStorage: 'json',
-    migrationStoragePath: path.resolve(__dirname, '../db/sequelizeMeta.json'),
-    seederStorage: 'json',
-    seederStoragePath: path.resolve(__dirname, '../db/sequelizeData.json'),
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: "tfc_survival_development",
+    host: "localhost",
+    dialect: "postgres",
+    migrationStorage: "json",
+    migrationStoragePath: path.resolve(__dirname, "../db/sequelizeMeta.json"),
+    seederStorage: "json",
+    seederStoragePath: path.resolve(__dirname, "../db/sequelizeData.json"),
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'tfc_survival_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: "tfc_survival_test",
+    host: "localhost",
+    dialect: "postgres",
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'tfc_survival_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: "tfc_survival_production",
+    host: "localhost",
+    dialect: "postgres",
   },
 };
