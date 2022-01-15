81d86f5ff12c549cc2758b451331fc6cdd8ce0fb
Evhenii Lapshinov <koenigstag@gmail.com>
2022-01-15 14:22:12 +0200

    Add nickname var to mail text

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -12,9 +12,9 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-module.exports.createActivationLink = (uuid) => {
+module.exports.createActivationLink = uuid => {
   return uuid + CONSTANTS.MAIL_LINK_SEPARATOR + Date.now();
-}
+};
 
 module.exports.checkMailExpire = async link => {
   const timestamp = link.split(CONSTANTS.MAIL_LINK_SEPARATOR)[1];
@@ -26,20 +26,23 @@ module.exports.checkMailExpire = async link => {
   return true;
 };
 
-module.exports.sendActivationMail = async (to, link) => {
+module.exports.sendActivationMail = async (to, link, nickname) => {
   const href = `${CONSTANTS.REDIRECT_URL}/activate-email/?link=${link}`;
+
+  const linkExpirationMinutes = CONSTANTS.MAIL_EXPIRATION_TIME / 1000 / 60;
 
   await transporter.sendMail({
     from: process.env.SMTP_USER,
     to,
-    subject: 'Активация аккаунта на сайте ' + CONSTANTS.REDIRECT_URL,
+    subject: `Активация аккаунта '${nickname}' на сайте ${CONSTANTS.REDIRECT_URL}`,
     text: '',
     html: `
-            <div>
-                <h1>Для активации перейдите по ссылке</h1>
-                <h2>Время жизни ссылки 10 минут</h2>
+            <article>
+                <h2>Регистрация аккаунта '${nickname}' на сайте ${CONSTANTS.REDIRECT_URL}</h2>
+                <p>Для активации перейдите по ссылке</p>
+                <strong>Время жизни ссылки ${linkExpirationMinutes} минут</strong>
                 <div><a href="${href}">${href}</a></div>
-            </div>
+            </article>
         `,
   });
 };
