77e69320c86bd4cae44e20f537099340a544a375
Yevhenii <koenigstag@gmail.com>
2022-01-22 23:03:19 +0200

    Add vk api

---
+++ src/controllers/common.controller.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const path = require("path");
+const fs = require("fs/promises");
+const { SERVER_FOLDER, VK_GROUP, VK_API_VERSION } = require("../constants");
+
+module.exports.getBannedPlayers = async (req, res, next) => {
+  try {
+    const bannedPlayers = await fs.readFile(
+      `${SERVER_FOLDER}/banned-players.json`
+    );
+    const list = JSON.parse(bannedPlayers) || [];
+
+    res.status(200).send({ data: list });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getVKNews = async (req, res, next) => {
+  try {
+    // получить новости из вк
+    const vkURL = `https://api.vk.com/method/wall.get?owner_id=-${VK_GROUP}&filter=owner&access_token=${process.env.VK_ACCESS_TOKEN}&v=${VK_API_VERSION}`;
+    const response = await axios.get(vkURL);
+
+    // отправить новости как данные
+    if (response.data) {
+      res.status(200).send(response.data.response.items);
+    }
+  } catch (error) {}
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,14 @@
-const path = require('path');
 const router = require('express').Router();
 const UserRouter = require('./user.router');
 const MediaRouter = require('./user.media.router');
 const AuthRouter = require('./auth.router');
-const { getBannedPlayers } = require('../controllers/user.controller');
+const CommonController = require('../controllers/common.controller');
 
 router.use('/auth', AuthRouter)
 router.use('/users', UserRouter);
 router.use('/media', MediaRouter);
 
-router.use('/banlist', getBannedPlayers);
-router.use('/map', (req, res, next) => {
-  try {
-
-    res.sendFile(path.resolve('/home/xelo/Desktop/server/dynmap/web/index.html'));
-  } catch (error) {
-    next(error);
-  }
-})
+router.use('/banlist', CommonController.getBannedPlayers);
+router.use('/vknews', CommonController.getVKNews);
 
 module.exports = router;
