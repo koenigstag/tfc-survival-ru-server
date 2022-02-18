fc8d4f4c2ba5a1f226b01857bec75312127a8951
Yevhenii <koenigstag@gmail.com>
2022-02-18 22:47:13 +0200

    Add migrate script

---
+++ misc/migrate-script/index.js
@@ -0,0 +1,56 @@
+const path = require("path");
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+const NODE_ENV = process.env.NODE_ENV;
+const DB_USER = process.env["DB_USER_" + NODE_ENV];
+const DB_PASSWORD = process.env["DB_PASSWORD_" + NODE_ENV];
+
+const mysql = require('mysql2');
+
+const bcrypt = require('bcrypt');
+
+const sourceConnect = mysql.createConnection({
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: 'localhost',
+  database: 'tfc',
+});
+
+const targetConnect = mysql.createConnection({
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: 'localhost',
+  database: 'tfc_survival_development',
+});
+
+// const limit = 10;
+const limit = 1;
+const countUsers = 2371;
+
+// for (let offset = 0; offset < countUsers; offset += limit) {
+for (let offset = 0; offset < 1; offset += limit) {
+  sourceConnect.query(
+    'SELECT * FROM Users ORDER BY Id LIMIT ? OFFSET ? ;',
+    [limit, offset],
+    function(err, results, fields) {
+      if (err) throw err;
+      // console.log(results); 
+      
+      for (const row of results) {
+        targetConnect.query(
+          `INSERT INTO Users 
+          (
+            nickname, password_hash, email, discord
+          ) 
+          VALUES 
+          (
+            ? , ? , ? , ?
+          );`, 
+          [row.Login, bcrypt.hashSync(row.Password, 6), row.Email, row.Discord], 
+          function (err) {
+          if (err) throw err;
+          }
+        )
+      }
+    }
+  );
+}
