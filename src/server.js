54d50cfa5eecef1b7b1ce56d852208883e2359ba
Yevhenii Lapshynov <eugeniy.lapshynov@freshcodeit.com>
2022-03-05 22:36:42 +0200

    Remove http server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const http = require("http");
+// const http = require("http");
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
@@ -6,19 +6,20 @@ require("dotenv").config({ path: path.resolve(__dirname + "/../.env") });
 const { log } = require("./misc/logger");
 const app = require("./app.js");
 
-const httpPort = process.env.PORT || 5001;
+// const httpPort = process.env.PORT || 5001;
 const httpsPort = process.env.HTTPS_PORT || 3001;
 
-const httpServer = http.createServer(app);
+/* const httpServer = http.createServer(app);
 
 httpServer.listen(httpPort, () => {
   log(`Http APP started on port ${httpPort}`);
-});
+}); */
 
 const httpsServer = https.createServer(
   {
     key: fs.readFileSync(path.resolve(__dirname, "../misc", "./ssl/private.key")), // путь к ключу
     cert: fs.readFileSync(path.resolve(__dirname, "../misc", "./ssl/domain_name.crt")), // путь к сертификату
+    ca: fs.readFileSync(path.resolve(__dirname, "../misc", "./ssl/chain.crt")), // путь к CA
   },
   app
 );
