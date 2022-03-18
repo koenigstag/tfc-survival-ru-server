c3d8f26421248b887a6f3e7035e56a911bb7b216
Yevhenii Lapshynov <eugeniy.lapshynov@freshcodeit.com>
2022-03-18 09:45:53 +0200

    Fix server domain

--- public/news/assets/js/constants.mjs
+++ public/news/assets/js/constants.mjs
@@ -1,6 +1,6 @@
 // CONSTANTS
-// window.SERVER_DOMAIN = 'https://tfc-survival.ru:3001';
-window.SERVER_DOMAIN = "http://localhost:5001";
+window.SERVER_DOMAIN = 'https://tfc-survival.ru:3001';
+// window.SERVER_DOMAIN = "http://localhost:5001";
 window.STATIC_NEWS_PATH = '/static/news'
 window.CLIENT_DOMAIN = "https://tfc-survival.ru";
 window.VK_GROUP_ID = "";
